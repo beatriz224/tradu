9b91db0dbf9ef0a3ab8606a0c72c7d79b4187f83
beatriz224 <beatriz.pedroso.silva2009@gmail.com>
2025-09-22 06:09:07 -0300

    Add files via upload

---
+++ js/traducao.js
@@ -0,0 +1,40 @@
+$function() {
+  $(".br").click(function(){
+    //cebeçalho//
+$(".nav-menu").children().eq(0).text("História");
+$(".nav-menu").children().eq(1).text("Repertório");
+$(".nav-menu").children().eq(2).text("Defesa");
+$(".language-selected").text("pt-Br");
+ $(".languagem-selected").addClass(".change-br");
+
+
+});
+};
+
+$function() {
+  $(".en").click(function(){
+    //cebeçalho//
+$(".nav-menu ul" ).children().eq(0).text("History");
+$(".nav-menu ul").children().eq(1).text("Repertoire");
+$(".nav-menu ul").children().eq(2).text("Defense");
+$(".language-selected").text("pt-en-US");
+ $(".languagem-selected").addClass(".change-en");
+$(".content-leftjk p:nth-child(1)")
+
+
+});
+};
+
+$function() {
+  $(".es").click(function(){
+    //cebeçalho//
+$(".nav-menu").children().eq(0).text("Historia");
+$(".nav-menu").children().eq(1).text("Repertorio");
+$(".nav-menu").children().eq(2).text("Defensa");
+$(".language-selected").text("pt-Br");
+ $(".languagem-selected").addClass(".change-es");
+
+
+
+});
+};
